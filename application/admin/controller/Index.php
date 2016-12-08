84171d83da1e7c36aab9e16a5e11c59fd8697d04
comeonkaikai <15075624661@163.com>
2016-12-08 22:21:06 +0800

    'yemian'

--- application/admin/controller/Index.php
+++ application/admin/controller/Index.php
@@ -6,8 +6,8 @@ class Index extends Auth
 {
 	public function index()
 	{
-		$list = User::select();
-		$soft = User::onlyTrashed()->select();
+		$list = User::paginate(2);
+		$soft = User::onlyTrashed()->paginate(2);
 		$this->assign('list',$list);
 		$this->assign('soft',$soft);
 		return $this->fetch();
